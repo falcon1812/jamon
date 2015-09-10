08c56b8f859ca9effc7c3ca3a7e5a05c4c48b1d1
matsuyuna <matsuyuna@users.noreply.github.com>
2015-09-10 09:37:20 -0430

    Update index.php

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
     <li><a href="#/page1">Pagina 1</a></li>
     <li><a href="#/page2">Pagina 2</a></li>
   </ul>
+    <hr>
   <div id="view"></div>
   <script>
   route('/', 'home', function () {});
